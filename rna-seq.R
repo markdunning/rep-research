0d2f9445517ee409b59fdae099e2a3688a5c7f7b
markdunning <mark.dunning@gmail.com>
2015-06-16 14:53:00 +0100

    initial commit

---
+++ rna-seq.R
@@ -0,0 +1,34 @@
+library(edgeR)
+
+datafile = system.file( "extdata/pasilla_gene_counts.tsv", package="pasilla" )
+datafile
+
+pasillaCountTable = read.table( datafile, header=TRUE, row.names=1 )
+
+head(pasillaCountTable)
+
+pasillaDesign = data.frame(
+  row.names = colnames( pasillaCountTable ),
+  condition = c( "untreated", "untreated", "untreated",
+                 "untreated", "treated", "treated", "treated" ),
+  libType = c( "single-end", "single-end", "paired-end",
+               "paired-end", "single-end", "paired-end", "paired-end" ) )
+
+
+pasillaDesign
+
+pairedSamples = pasillaDesign$libType == "paired-end"
+countTable = pasillaCountTable[ , pairedSamples ]
+condition = pasillaDesign$condition[ pairedSamples ]
+
+y <- DGEList(counts=countTable,group=condition)
+y <- calcNormFactors(y)
+y <- estimateCommonDisp(y)
+y <- estimateTagwiseDisp(y)
+et <- exactTest(y)
+topTags(et)
+
+
+pasillaRes <- topTags(et,n=nrow(countTable))$table
+
+sessionInfo()
